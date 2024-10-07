cf30c1434fbec84cf292e4b8ed042ecd2ba99b49
Paolo Preti <PAOLO.PRETI@local.ittcesena.it>
2024-10-07 09:42:48 +0200

    release 0.0.1: inizio della classe CD

---
+++ Esercitazione_GIT_4H/CD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercitazione_GIT_4H
+{
+    internal class CD
+    {
+        private string titolo, autore;
+        private List<Brano> brani = new List<Brano>();
+
+        public CD(string titolo, string autore, List<Brano> brani)
+        {
+            this.titolo = titolo;
+            this.autore = autore;
+            this.brani = brani;
+        }
+
+
+        public string toString(List<Brano> brani)
+        {
+            string listaDiBrani = "";
+            for (int i = 0; i < brani.Count; i++)
+            {
+                listaDiBrani += " " + brani[i].getTitolo();
+            }
+            return listaDiBrani;
+        }
+    }
+}
